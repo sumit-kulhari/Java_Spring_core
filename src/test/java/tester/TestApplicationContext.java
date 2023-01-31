5612dec9821ae0a8f3e91593802685de854a1cac
Sumit Kulhari <sk4213@srmist.edu.in>
2023-01-31 15:37:04 +0530

    Updated Employeetest to TestApplicationContext

--- src/test/java/tester/TestApplicationContext.java
+++ src/test/java/tester/TestApplicationContext.java
@@ -4,7 +4,7 @@ import com.bhavana.beans.Employee;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class TestEmployee {
+public class TestApplicationContext {
     public static void main(String args[]){
         ApplicationContext ac = new ClassPathXmlApplicationContext("bhavana_config.xml");
         Employee employee = (Employee)ac.getBean("employee");
